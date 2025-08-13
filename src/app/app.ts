99e95ae54222529a1efb82f993f6a5f180fb7a7a
sayranovv <sayranoveldar@gmail.com>
2025-08-14 00:42:34 +0500

    Add API get query

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
-import { Component, signal } from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ProfileCard} from './common-ui/profile-card/profile-card';
+import {ProfileService} from './data/services/profile';
+import {Profile} from './data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,10 @@ import {ProfileCard} from './common-ui/profile-card/profile-card';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('tik-talk');
+  profileService = inject(ProfileService)
+  profiles: Profile[] = []
+
+  constructor() {
+    this.profileService.getTestAccounts().subscribe(val => this.profiles = val)
+  }
 }
--- src/app/common-ui/profile-card/profile-card.ts
+++ src/app/common-ui/profile-card/profile-card.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Profile} from '../../data/interfaces/profile.interface';
+import {ImgUrlPipe} from '../../helpers/pipes/img-url-pipe';
 
 @Component({
   selector: 'app-profile-card',
-  imports: [],
+  imports: [
+    ImgUrlPipe
+  ],
   templateUrl: './profile-card.html',
   styleUrl: './profile-card.scss'
 })
 export class ProfileCard {
-
+  @Input() profile!: Profile
 }
