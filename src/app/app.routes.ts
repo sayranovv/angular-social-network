4994eea4a634047bddf2acd4f6379c7d3dc3a00a
sayranovv <sayranoveldar@gmail.com>
2025-08-14 13:16:22 +0500

    Add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {LoginPage} from './pages/login-page/login-page';
+import {SearchPage} from './pages/search-page/search-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: SearchPage },
+  { path: 'login', component: LoginPage },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,12 @@
-import {Component, inject, signal} from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {ProfileCard} from './common-ui/profile-card/profile-card';
-import {ProfileService} from './data/services/profile';
-import {Profile} from './data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileCard],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  profileService = inject(ProfileService)
-  profiles: Profile[] = []
 
-  constructor() {
-    this.profileService.getTestAccounts().subscribe(val => this.profiles = val)
-  }
 }
