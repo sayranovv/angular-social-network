47c5f241400b9ea3a136fab238e08aca416d561d
sayranovv <sayranoveldar@gmail.com>
2025-08-20 02:23:23 +0500

    Create sidebar

--- src/app/common-ui/sidebar/sidebar.ts
+++ src/app/common-ui/sidebar/sidebar.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, inject, WritableSignal} from '@angular/core';
+import {SvgIcon} from '../svg-icon/svg-icon';
+import {SubscriberCard} from './subscriber-card/subscriber-card';
+import {RouterLink} from '@angular/router';
+import {ProfileService} from '../../data/services/profile';
+import {AsyncPipe, JsonPipe} from '@angular/common';
+import {firstValueFrom} from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [
+    SvgIcon,
+    SubscriberCard,
+    RouterLink,
+    AsyncPipe,
+  ],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss'
 })
 export class Sidebar {
+  profileService = inject(ProfileService)
 
+  subscribers$ = this.profileService.getSubscribersShortList()
+
+  me = this.profileService.me
+
+  menuItems = [
+    {
+      label: 'Моя страница',
+      icon: 'home',
+      link: ''
+    },
+    {
+      label: 'Чаты',
+      icon: 'chat',
+      link: 'chat'
+    },
+    {
+      label: 'Поиск',
+      icon: 'search',
+      link: 'search'
+    },
+  ]
+
+  ngOnInit() {
+    firstValueFrom(this.profileService.getMe())
+  }
 }
