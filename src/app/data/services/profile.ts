d46ebd24ef55a91b8c7004814d587edb904057cf
sayranovv <sayranoveldar@gmail.com>
2025-09-04 11:30:17 +0500

    Add filters components to search page

--- src/app/data/services/profile.ts
+++ src/app/data/services/profile.ts
@@ -12,6 +12,7 @@ export class ProfileService {
   baseApiUrl = 'https://icherniakov.ru/yt-course/'
 
   me = signal<Profile | null>(null)
+  filteredProfiles = signal<Profile[]>([])
 
   getTestAccounts() {
     return this.http.get<Profile[]>(`${this.baseApiUrl}account/test_accounts`)
@@ -40,5 +41,15 @@ export class ProfileService {
     const fd = new FormData()
     fd.append('image', file)
     return this.http.post<Profile>(`${this.baseApiUrl}account/upload_image`, fd)
+  }
+
+  filterProfiles(params: Record<string, any>){
+    return this.http.get<Pageble<Profile>>(`${this.baseApiUrl}account/accounts`,
+      {
+        params
+      }
+    ).pipe(
+      tap(res => this.filteredProfiles.set(res.items))
+    )
   }
 }
